4ba342a5b891f74e4dedf5e2c630baa3853db834
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-24 00:32:11 +0300

    task 1

--- sort.h
+++ sort.h
@@ -20,7 +20,9 @@ typedef struct listint_s
 /*Printing helper */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+/**Helper functions**/
+void swap(listint_t *a, listint_t *b)
 /** prototypes of the functions **/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif /*_SORTING_H*/
